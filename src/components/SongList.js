c3e00ac678f21383605779b24599361b8b32b958
Ryan Kerins <ryanakerins@gmail.com>
2019-03-13 10:05:57 -0400

    fix: removed console logs

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -6,13 +6,11 @@ import {connect} from 'react-redux'
 class SongList extends React.Component {
   constructor(props) {
     super(props)
-    // console.log(props)
   }
 
   render() {
   return (
     <div id={this.props.id}>
-    {/* {console.log('look at props here', this.props)} */}
       {this.props.songs.map((song, idx) => {
         return <Song key={idx} title={song[0]} number={idx + 1} artist={song[2]}/>
       })}
